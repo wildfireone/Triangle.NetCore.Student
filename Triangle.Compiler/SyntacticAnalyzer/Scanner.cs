0e1c959270dcc237a5fa6f4d54860dc8d9d9077c
john <wildfireone@gmail.com>
2017-10-12 16:16:24 +0100

    week 3 lab

--- Triangle.Compiler/SyntacticAnalyzer/Scanner.cs
+++ Triangle.Compiler/SyntacticAnalyzer/Scanner.cs
@@ -57,200 +57,30 @@ namespace Triangle.Compiler.SyntacticAnalyzer
             return GetEnumerator();
         }
 
-        /// <summary>
-        /// Appends the current character to the current token, and gets
-        /// the next character from the source program.
-        /// </summary>
+     
+        // Appends the current character to the current token, and gets
+        //the next character from the source program.
+
         void TakeIt()
         {
             _currentSpelling.Append((char)_source.Current);
             _source.MoveNext();
         }
 
-        /// <summary>
-        /// Skip a single separator.
-        /// </summary>
+
+        //Skip a single separator.
+       
         void ScanSeparator()
         {
-            switch (_source.Current)
-            {
-                case '!':
-                    _source.SkipRestOfLine();
-                    _source.MoveNext();
-                    break;
-
-                case ' ':
-                case '\n':
-                case '\r':
-                case '\t':
-                    _source.MoveNext();
-                    break;
-            }
+            
         }
 
-        TokenKind ScanToken()
+		//Build up tokens.
+		TokenKind ScanToken()
         {
 
             switch (_source.Current)
             {
-
-                case 'a':
-                case 'b':
-                case 'c':
-                case 'd':
-                case 'e':
-                case 'f':
-                case 'g':
-                case 'h':
-                case 'i':
-                case 'j':
-                case 'k':
-                case 'l':
-                case 'm':
-                case 'n':
-                case 'o':
-                case 'p':
-                case 'q':
-                case 'r':
-                case 's':
-                case 't':
-                case 'u':
-                case 'v':
-                case 'w':
-                case 'x':
-                case 'y':
-                case 'z':
-                case 'A':
-                case 'B':
-                case 'C':
-                case 'D':
-                case 'E':
-                case 'F':
-                case 'G':
-                case 'H':
-                case 'I':
-                case 'J':
-                case 'K':
-                case 'L':
-                case 'M':
-                case 'N':
-                case 'O':
-                case 'P':
-                case 'Q':
-                case 'R':
-                case 'S':
-                case 'T':
-                case 'U':
-                case 'V':
-                case 'W':
-                case 'X':
-                case 'Y':
-                case 'Z':
-                    TakeIt();
-                    while (IsLetter(_source.Current) || IsDigit(_source.Current))
-                    {
-                        TakeIt();
-                    }
-                    return TokenKind.Identifier;
-
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    TakeIt();
-                    while (IsDigit(_source.Current))
-                    {
-                        TakeIt();
-                    }
-                    return TokenKind.IntLiteral;
-
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '=':
-                case '<':
-                case '>':
-                case '\\':
-                case '&':
-                case '@':
-                case '%':
-                case '^':
-                case '?':
-                    TakeIt();
-                    while (IsOperator(_source.Current))
-                    {
-                        TakeIt();
-                    }
-                    return TokenKind.Operator;
-
-                case '\'':
-                    TakeIt();
-                    TakeIt(); // the quoted character
-                    if (_source.Current == '\'')
-                    {
-                        TakeIt();
-                        return TokenKind.CharLiteral;
-                    }
-                    return TokenKind.Error;
-
-                case '.':
-                    TakeIt();
-                    return TokenKind.Dot;
-
-                case ':':
-                    TakeIt();
-                    if (_source.Current == '=')
-                    {
-                        TakeIt();
-                        return TokenKind.Becomes;
-                    }
-                    return TokenKind.Colon;
-
-                case ';':
-                    TakeIt();
-                    return TokenKind.Semicolon;
-
-                case ',':
-                    TakeIt();
-                    return TokenKind.Comma;
-
-                case '~':
-                    TakeIt();
-                    return TokenKind.Is;
-
-                case '(':
-                    TakeIt();
-                    return TokenKind.LeftParen;
-
-                case ')':
-                    TakeIt();
-                    return TokenKind.RightParen;
-
-                case '[':
-                    TakeIt();
-                    return TokenKind.LeftBracket;
-
-                case ']':
-                    TakeIt();
-                    return TokenKind.RightBracket;
-
-                case '{':
-                    TakeIt();
-                    return TokenKind.LeftCurly;
-
-                case '}':
-                    TakeIt();
-                    return TokenKind.RightCurly;
-
-                case -1:
-                    return TokenKind.EndOfText;
 
                 default:
                     TakeIt();
